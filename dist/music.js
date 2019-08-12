8f720f788396eb83d2ae86ba7eff1e6dbc9e9c34
xiaomai <codexiaomai@foxmail.com>
2019-08-12 21:54:24 +0800

    Site updated: 2019-08-12 21:54:24

--- dist/music.js
+++ dist/music.js
@@ -6,13 +6,13 @@ const ap = new APlayer({
       {
         name: '爱要怎么说出口',
         artist: 'JJ Lin',
-        url: 'http://fs.open.kugou.com/1b8ec0add2d0c335ab9a15d523f0e692/5d4ae016/G086/M09/0F/1B/NpQEAFjCpyGAZiCFADoCyOqZemU749.mp3',
+        url: 'https://xiaomai.cf/music/爱要怎么说出口.mp3',
         cover: '/images/ljj.jpg',
       },
       {
         name: '我们的爱',
         artist: 'JJ Lin',
-        url: 'http://fs.open.kugou.com/b2bab048d52dbd6e34a3f51e5ffb3763/5d4ae0a9/G123/M05/0F/05/uw0DAFpYmGqAOprmAFU1LSRVygY182.mp3',
+        url: 'https://xiaomai.cf/music/我们的爱.mp3',
         cover: '/images/ljj.jpg',
       }
     ]
